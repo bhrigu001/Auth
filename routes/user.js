1d4cc72428aa4e964e68b473b720df331f523dd1
bhrigu001 <mishrabhrigu2003@gmail.com>
2024-02-15 10:47:13 +0530

    Login handler completed and middlewares added

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,34 @@ const express = require('express');
 const router = express.Router();
 
 const {login,signup} = require("../Controllers/Auth");
+const {auth,isStudent,isAdmin} = require("../middlewares/auth"); 
 
-// router.post("/login",login);
+router.post("/login",login);
 router.post("/signup",signup);
+
+
+//Testing Route for single middleware
+router.get("/test",auth,(req,res)=>{
+    res.json({
+        success:true,
+        message:'Welcome to the test Route',
+    });
+});
+
+//Protected Route
+router.get("/student",auth,isStudent,(req,res)=>{
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for Students',
+    });
+});
+
+router.get("/admin",auth,isAdmin,(req,res)=>{
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for admin',
+    });
+});
+
 
 module.exports = router;
